7307b6949482f1c0ecf8f8fd41b559b29762c0ce
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-27 20:51:27 +0700

    difference between template and reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
